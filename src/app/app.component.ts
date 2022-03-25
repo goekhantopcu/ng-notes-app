29b7c0782baf90df1a5e3df2606689c0172ea44c
Gökhan Topcu <64787457+JailBreakerTV@users.noreply.github.com>
2022-03-25 07:56:24 +0100

    feat: add ability to list all existing notes, remove them or add new to the notes list

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,6 @@
-import { Component } from '@angular/core';
+import {Component} from '@angular/core';
+import {Note} from "./note";
+import {FormControl, FormGroup} from "@angular/forms";
 
 @Component({
   selector: 'app-root',
@@ -7,4 +9,27 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'ng-notes-app';
+
+  titleControl = new FormControl();
+  contentControl = new FormControl();
+
+  notes: Note[] = [
+    {title: 'Lernen', content: 'Für die Prüfung x am xx.xx.xxxx lernen', date: Date.now()},
+    {title: 'Essen', content: 'Hähnchen mit Reis zubereiten', date: Date.now()},
+    {title: 'Gym', content: 'Ins Gym mit den Jungs gehen', date: Date.now()},
+  ]
+
+  doRemoveNote(remove: Note) {
+    this.notes = this.notes.filter(note => note.title !== remove.title);
+  }
+
+  doAddNote() {
+    this.notes.push(
+      {
+        title: this.titleControl.value,
+        content: this.contentControl.value,
+        date: Date.now()
+      }
+    );
+  }
 }
---
+++ src/app/note.ts
@@ -0,0 +1,5 @@
+export interface Note {
+  title: String,
+  content: String,
+  date: number
+}
